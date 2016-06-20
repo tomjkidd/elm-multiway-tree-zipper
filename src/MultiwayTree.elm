9a1af463332cca20a2e6bc87f6f684769134b17d
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2016-06-20 14:48:17 +0200

    elm-format, adds foldl foldr flatten

--- src/MultiwayTree.elm
+++ src/MultiwayTree.elm
@@ -1,8 +1,14 @@
-module MultiwayTree exposing
-    ( Tree (..), Forest
-    , datum, children
-    , map
-    )
+module MultiwayTree
+    exposing
+        ( Tree(..)
+        , Forest
+        , datum
+        , children
+        , map
+        , flatten
+        , foldr
+        , foldl
+        )
 
 {-| A library for constructing multi-way trees. Each Tree carries two pieces of
 information, it's datum and children.
@@ -18,33 +24,71 @@ information, it's datum and children.
 @docs map
 -}
 
+
 {-| A type to keep track of datum and children.
 -}
-type Tree a = Tree a (Forest a)
+type Tree a
+    = Tree a (Forest a)
 
 
 {-| A list of Trees. Convenient for describing children.
 -}
-type alias Forest a = List (Tree a)
+type alias Forest a =
+    List (Tree a)
 
 
 {-| Access the datum of the current tree
 -}
 datum : Tree a -> a
-datum (Tree datum children) = datum
+datum (Tree datum children) =
+    datum
 
 
 {-| Access the children of the current tree
 -}
 children : Tree a -> Forest a
-children (Tree datum children) = children
+children (Tree datum children) =
+    children
+
+
+{-| Reduce a Tree from the left.
+-}
+foldl : (a -> b -> b) -> b -> Tree a -> b
+foldl f accu (Tree datum children) =
+    let
+        treeUnwrap (Tree datum' children') accu' =
+            List.foldl treeUnwrap (f datum' accu') children'
+    in
+        List.foldl treeUnwrap (f datum accu) children
+
+
+{-| Reduce a Tree from the right.
+-}
+foldr : (a -> b -> b) -> b -> Tree a -> b
+foldr f accu (Tree datum children) =
+    let
+        treeUnwrap (Tree datum' children') accu' =
+            f datum' (List.foldr treeUnwrap accu' children')
+    in
+        f datum (List.foldr treeUnwrap accu children)
+
+
+{-| Flattens a Tree into a List where the root is the first element of that list.
+-}
+flatten : Tree a -> List a
+flatten tree =
+    foldr (::) [] tree
 
 
 {-| Map over the MultiwayTree
 -}
 map : (a -> b) -> Tree a -> Tree b
 map fn (Tree datum children) =
-    let mappedDatum = fn datum
-        mappedChildren = List.map (\child -> map fn child) children
+    let
+        mappedDatum =
+            fn datum
+
+        mappedChildren =
+            List.map (\child -> map fn child) children
     in
         (Tree mappedDatum mappedChildren)
---
+++ tests/Test/FoldTests.elm
@@ -0,0 +1,23 @@
+module Test.FoldTests exposing (..)
+
+import ElmTest exposing (..)
+import MultiwayTree exposing (Tree(..))
+import MultiwayTreeZipper exposing (..)
+import Test.SampleData
+    exposing
+        ( noChildTree
+        , singleChildTree
+        , multiChildTree
+        , deepTree
+        , noChildRecord
+        , interestingTree
+        )
+
+
+tests : Test
+tests =
+    suite "Fold"
+        [ test "Foldl interestingTree into List"
+            <| assertEqual (MultiwayTree.flatten interestingTree)
+                ((MultiwayTree.foldl (::) [] interestingTree) |> List.reverse)
+        ]
