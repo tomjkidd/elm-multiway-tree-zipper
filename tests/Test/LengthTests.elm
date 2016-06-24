8357b3dca4cd0c94a65837fade58720d8ba59d88
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2016-06-24 18:02:17 +0200

    add map2, indexedMap and length

---
+++ tests/Test/LengthTests.elm
@@ -0,0 +1,32 @@
+module Test.LengthTests exposing (..)
+
+import ElmTest exposing (..)
+import MultiwayTree exposing (Tree(..))
+import MultiwayTreeZipper exposing (..)
+import Test.SampleData
+    exposing
+        ( noChildTree
+        , singleChildTree
+        , multiChildTree
+        , deepTree
+        , noChildRecord
+        , interestingTree
+        )
+
+
+tests : Test
+tests =
+    suite "Length"
+        [ test "Length of an interesting Tree"
+            <| assertEqual 11
+                (MultiwayTree.length interestingTree)
+        , test "Length of a noChildTree"
+            <| assertEqual 1
+                (MultiwayTree.length noChildTree)
+        , test "Length of a deepTree"
+            <| assertEqual 4
+                (MultiwayTree.length deepTree)
+        , test "Length of a Tree is equal to length of a flattened tree"
+            <| assertEqual (List.length (MultiwayTree.flatten interestingTree))
+                (MultiwayTree.length interestingTree)
+        ]
