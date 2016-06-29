058b7f036f4eb95b8d728f33782f3a6310e65164
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2016-06-29 18:26:29 +0200

    add tuplesOfDatumAndFlatChildren function

--- tests/Test/MultiwayTreeZipper.elm
+++ tests/Test/MultiwayTreeZipper.elm
@@ -14,6 +14,7 @@ import Test.AppendTests
 import Test.InsertTests
 import Test.LengthTests
 import Test.IndexedMapTests
+import Test.TuplesOfDatumAndFlatChildrenTests
 
 
 (&>) =
@@ -33,4 +34,5 @@ tests =
         , Test.InsertTests.tests
         , Test.LengthTests.tests
         , Test.IndexedMapTests.tests
+        , Test.TuplesOfDatumAndFlatChildrenTests.tests
         ]
